c6847fc07cc3717689a9e434f9df7018be07a267
Alex Torres <torres.alex.at04@gmail.com>
2023-11-27 10:22:00 +0000

    indoOnde?

---
+++ IndoOnde/indoali.js
@@ -0,0 +1,4 @@
+document.querySelector(".button").addEventListener("click", function () {
+    document.querySelector(".button").style.opacity = "0%";
+    document.querySelector(".quadradonaoquadrado").style.opacity = "100%";
+});
